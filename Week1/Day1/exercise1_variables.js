b68b47dc94e22c3cc92b05810e64e2599e2ed0bc
dev-mabdulrehman <dev.mabdulrehman@gmail.com>
2025-11-10 09:40:25 +0000

    Day1 - Exercise1: Variable declarations and  reassignment

---
+++ Week1/Day1/exercise1_variables.js
@@ -0,0 +1,11 @@
+// Exercise 1 - Variable declaration Exercise
+
+var x = 100;
+let y = 200;
+const z = 300;
+
+x = 150;
+y = 250;
+//z = 350; // TypeError: Assignment to constant variable.
+
+console.log(x, y, z);
